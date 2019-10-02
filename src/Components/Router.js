ebcf05dd172c072fdd11d67c6db45c18d7a5c796
jhs851 <jhs851@naver.com>
2019-10-02 16:42:16 +0900

    #5.4 requestSecret Mutation and Refactor

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth from '../Routes/Auth/index';
 import Feed from '../Routes/Feed';
-import Auth from '../Routes/Auth';
 
 const LoggedInRoutes = () => <><Route exact path="/" component={Feed} /></>;
 
