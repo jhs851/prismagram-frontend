5e20f8d2b277db5b369f49740f42b2f29fb84bee
jhs851 <jhs851@naver.com>
2019-10-04 14:58:57 +0900

    #5.8 confirmSecret + Log In Mutation

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AuthPresenter from './AuthPresenter';
 import useInput from '../../Hooks/useInput';
 import { useMutation } from 'react-apollo-hooks';
-import { CREATE_ACCOUNT, LOG_IN } from './AuthQueries';
+import { CONFIRM_SECRET, CREATE_ACCOUNT, LOCAL_LOG_IN, LOG_IN } from './AuthQueries';
 import { toast } from 'react-toastify';
 
 export default () => {
@@ -23,6 +23,13 @@ export default () => {
             lastName: lastName.value
         }
     });
+    const [confirmSecretMutation] = useMutation(CONFIRM_SECRET, {
+        variables: {
+            email: email.value,
+            secret: secret.value
+        }
+    });
+    const [localLogInMutation] = useMutation(LOCAL_LOG_IN);
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -66,6 +73,24 @@ export default () => {
                 }
             } else {
                 toast.error('All field are required');
+            }
+        } else if (action === 'confirm') {
+            if (secret.value !== '') {
+                try {
+                    const {
+                        data : { confirmSecret: token }
+                    } = await confirmSecretMutation();
+
+                    if (token !== '' && token !== undefined) {
+                        localLogInMutation({
+                            variables: { token }
+                        });
+                    } else {
+                        throw Error();
+                    }
+                } catch (e) {
+                    toast.error('Can\'t confirm secret, check again');
+                }
             }
         }
     };
