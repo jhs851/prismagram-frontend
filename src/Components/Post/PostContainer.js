0056bf5ba4fc33bb1a655948180504e196db37c3
jhs851 <jhs851@naver.com>
2019-10-11 18:26:48 +0900

    #7.3 Post Component part Three

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import useInput from '../../Hooks/useInput';
 import PostPresenter from './PostPresenter';
@@ -6,7 +6,18 @@ import PostPresenter from './PostPresenter';
 const PostContainer = ({ id, user, files, likeCount, isLiked, comments, createdAt, caption, location }) => {
     const [isLikedS, setIsLiked] = useState(isLiked);
     const [likeCountS, setLikeCount] = useState(likeCount);
+    const [currentItem, setCurrentItem] = useState(0);
     const comment = useInput('');
+    const slide = () => {
+        const totalFiles = files.length;
+
+        if (currentItem === totalFiles - 1) {
+            setTimeout(() => setCurrentItem(0), 3000);
+        } else {
+            setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+        }
+    };
+    useEffect(slide, [currentItem]);
 
     return (
         <PostPresenter
@@ -21,6 +32,7 @@ const PostContainer = ({ id, user, files, likeCount, isLiked, comments, createdA
             newComment={comment}
             setIsLiked={setIsLiked}
             setLikeCount={setLikeCount}
+            currentItem={currentItem}
         />
     );
 };
