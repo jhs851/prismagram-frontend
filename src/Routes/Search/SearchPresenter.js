26b8b35f1916bfdad79c49a3b89b1208fbde5453
jhs851 <jhs851@naver.com>
2019-10-17 17:14:30 +0900

    #8.1 Search Screen UI Part One

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,16 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import FatText from '../../Components/FatText';
+import Loader from '../../Components/Loader';
+import UserCard from '../../Components/UserCard';
 
 const Wrapper = styled.div`
     height: 50vh;
-    text-align: center;
 `;
 
-const SearchPresenter = ({ term, loading }) => {
+const Section = styled.div``;
+
+const SearchPresenter = ({ term, loading, data }) => {
+    if (term === undefined) {
+        return <Wrapper><FatText text="Search for someting" /></Wrapper>;
+    } else if (loading) {
+        return <Wrapper><Loader /></Wrapper>;
+    } else if (
+        data &&
+        (data.searchUser && data.searchUser.length === 0) ||
+        (data.searchPost && data.searchPost.length === 0)
+    ) { 
+        return <Wrapper><FatText text="No Users, Photos Found"/></Wrapper>;
+    }
+
     return (
         <Wrapper>
-            { term === undefined && <FatText text="Search for something" /> }
+            <Section>
+                {data.searchUser.map(user =>
+                    <UserCard url={user.url}
+                              isFollowing={user.isFollowing}
+                              isSelf={user.isSelf}
+                              username={user.username}
+                    />
+                )}
+            </Section>
+
+            <Section>
+                {data.searchPost.map(user =>
+                    <UserCard url={user.url}
+                              isFollowing={user.isFollowing}
+                              isSelf={user.isSelf}
+                              username={user.username}
+                    />
+                )}
+            </Section>
         </Wrapper>
     );
 };
